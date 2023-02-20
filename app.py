79702f18c3885ce9cdc3afc03e4069988c1fb0d8
Nicolas Goetschel <nicgoe@hotmail.com>
2023-02-20 08:46:55 +0000

    app.py updated

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
+from flask import Flask, render_template, request, redirect
 
-from flask import Flask
+from controllers.country_controller import countries_blueprint
+from controllers.league_controller import leagues_blueprint
+from controllers.ground_controller import grounds_blueprint
 
 app = Flask(__name__)
 
-from controllers import controller
+app.register_blueprint(countries_blueprint)
+app.register_blueprint(leagues_blueprint)
+app.register_blueprint(grounds_blueprint)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
